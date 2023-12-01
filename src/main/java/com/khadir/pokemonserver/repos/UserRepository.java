a88b0e65f8e9217a883f6c5cbee35cbfad719865
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-01 19:01:35 +0000

    Added user repository with custom findByUsername query

---
+++ src/main/java/com/khadir/pokemonserver/repos/UserRepository.java
@@ -0,0 +1,14 @@
+package com.khadir.pokemonserver.repos;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.khadir.pokemonserver.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	// custom queries!!
+    Optional<User> findByUsername(String username);
+}
