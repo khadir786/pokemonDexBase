11ef39b28ad69e844304a3cb375442979d4d41b8
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-01 18:53:33 +0000

    added user entity

---
+++ src/main/java/com/khadir/pokemonserver/models/User.java
@@ -0,0 +1,40 @@
+package com.khadir.pokemonserver.models;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor	
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+	private String password;
+	
+	@CreationTimestamp
+	private LocalDateTime createdOn;
+	
+	@UpdateTimestamp
+	private LocalDateTime updatedOn;
+	
+	
+}
