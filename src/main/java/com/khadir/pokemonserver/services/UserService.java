5198a616b4781e00827fd8b925744fa0321250cf
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-03 23:25:39 +0000

    created exception and exception handler for already existing users

--- src/main/java/com/khadir/pokemonserver/services/UserService.java
+++ src/main/java/com/khadir/pokemonserver/services/UserService.java
@@ -1,10 +1,11 @@
 package com.khadir.pokemonserver.services;
 
 import com.khadir.pokemonserver.dtos.UserDto;
+import com.khadir.pokemonserver.exceptions.UserAlreadyExistsException;
 import com.khadir.pokemonserver.models.User;
 
 public interface UserService {
-    User registerNewUserAccount(UserDto userDto);
+    User registerNewUserAccount(UserDto userDto) throws UserAlreadyExistsException;
     User getUserById(Long id);
     User updateUser(UserDto userDto, Long id);
     void deleteUser(Long id);
