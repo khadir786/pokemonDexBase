3ff39b5143dadc41c906b3d462e73e939d6f5146
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-04 00:09:26 +0000

    changed method name

--- src/main/java/com/khadir/pokemonserver/services/UserService.java
+++ src/main/java/com/khadir/pokemonserver/services/UserService.java
@@ -5,7 +5,7 @@ import com.khadir.pokemonserver.exceptions.UserAlreadyExistsException;
 import com.khadir.pokemonserver.models.User;
 
 public interface UserService {
-    User registerNewUserAccount(UserDto userDto) throws UserAlreadyExistsException;
+    User registerNewUser(UserDto userDto) throws UserAlreadyExistsException;
     User getUserById(Long id);
     User updateUser(UserDto userDto, Long id);
     void deleteUser(Long id);
