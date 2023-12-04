fffdc143b26be6c9a0613a90af45c220d35c5605
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-04 00:16:27 +0000

    added registerNewUser method to implementation

---
+++ src/main/java/com/khadir/pokemonserver/services/imps/UserServiceImpl.java
@@ -0,0 +1,65 @@
+package com.khadir.pokemonserver.services.imps;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Builder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.khadir.pokemonserver.dtos.UserDto;
+import com.khadir.pokemonserver.exceptions.UserAlreadyExistsException;
+import com.khadir.pokemonserver.models.User;
+import com.khadir.pokemonserver.repos.UserRepository;
+import com.khadir.pokemonserver.services.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public User registerNewUser(UserDto userDto) throws UserAlreadyExistsException {
+    	String username = userDto.getUsername();
+		if (userRepository.findByUsername(username).isPresent()) {
+			throw new UserAlreadyExistsException("There is already a user with the username: " + userDto.getUsername());
+		}
+    	
+		// Creating a new user instance
+		String encodedPassword = passwordEncoder.encode(userDto.getPassword());
+	    User newUser = User.builder()
+	    		.username(username)
+	    		.password(encodedPassword)
+	    		.build();
+
+	    // Additional fields can be set based on User class and UserDto
+
+	    return userRepository.save(newUser);
+    }
+
+
+	@Override
+	public User getUserById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User updateUser(UserDto userDto, Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUser(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+    // Implement other methods...
+}
