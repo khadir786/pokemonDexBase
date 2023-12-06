d20106d4268fd8897634c8c245c448e2cd07482c
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-06 14:52:01 +0000

    Created UserNotFound custom exception and added it to global exception handler

--- src/main/java/com/khadir/pokemonserver/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/khadir/pokemonserver/exceptions/GlobalExceptionHandler.java
@@ -14,4 +14,10 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ex.getMessage());
     }
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+    	return ResponseEntity
+    			.status(HttpStatus.BAD_REQUEST)
+    			.body(ex.getMessage());
+    }
 }
