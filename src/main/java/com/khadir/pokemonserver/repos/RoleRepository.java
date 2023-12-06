e60538de790b26db470efe2e0959d91aa34d71a6
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-06 17:45:28 +0000

    added role repository

---
+++ src/main/java/com/khadir/pokemonserver/repos/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.khadir.pokemonserver.repos;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.khadir.pokemonserver.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+	Role findByName(String name);
+}
