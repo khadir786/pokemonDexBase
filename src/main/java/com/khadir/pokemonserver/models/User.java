b8a46961512c2b934b2a90edb6a8d081acf00e64
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-09 17:41:58 +0000

    added not empty constraints to username and password

--- src/main/java/com/khadir/pokemonserver/models/User.java
+++ src/main/java/com/khadir/pokemonserver/models/User.java
@@ -18,6 +18,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -35,8 +36,9 @@ public class User {
     private Long id;
 
     @Column(unique = true)
+    @NotEmpty
     private String username;
-
+    @NotEmpty
 	private String password;
 	
 	@CreationTimestamp
