69ae292df34ece3470f8f49c555638249b2e5aa6
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-09 17:42:47 +0000

    allowed CORS for front end origin

--- src/main/java/com/khadir/pokemonserver/config/SecurityConfig.java
+++ src/main/java/com/khadir/pokemonserver/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 
@@ -25,6 +27,17 @@ public class SecurityConfig {
 			);
 		return http.build();
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+	        }
+	    };
+	}
+
 	
     @Bean
     public PasswordEncoder passwordEncoder() {
