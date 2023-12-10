6aecb175cecc98a03d2569c129b0eea8469140cd
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-10 17:44:33 +0000

    Added id to login response

--- src/main/java/com/khadir/pokemonserver/config/handlers/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/khadir/pokemonserver/config/handlers/CustomAuthenticationSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.khadir.pokemonserver.config.handlers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -7,22 +8,29 @@ import org.springframework.stereotype.Component;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.khadir.pokemonserver.dtos.UserResponseDto;
+import com.khadir.pokemonserver.models.User;
+import com.khadir.pokemonserver.repos.UserRepository;
 
 @Component
 public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         response.setStatus(HttpServletResponse.SC_OK);
 
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-
+        User user = userRepository.findByUsername(userDetails.getUsername()).get();
         // Create a response DTO
         UserResponseDto responseDto = UserResponseDto.builder()
+        		.id(user.getId())
                 .username(userDetails.getUsername())
                 .build();
 
