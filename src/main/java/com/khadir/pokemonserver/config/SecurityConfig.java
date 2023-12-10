df9f3e0928aecfbefc538f8d2ab6cb7b1ad79f00
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-10 17:12:52 +0000

    fixed cors configuration

--- src/main/java/com/khadir/pokemonserver/config/SecurityConfig.java
+++ src/main/java/com/khadir/pokemonserver/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.khadir.pokemonserver.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -38,6 +43,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
 		http
+			.cors(cors -> cors.configurationSource(myWebsiteConfigurationSource()))
 			.csrf(csrf -> csrf.disable())
 			.authorizeHttpRequests(authorize -> 
 			authorize
@@ -53,15 +59,15 @@ public class SecurityConfig {
 	}
 	
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-	    return new WebMvcConfigurer() {
-	        @Override
-	        public void addCorsMappings(CorsRegistry registry) {
-	            registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
-	        }
-	    };
+	CorsConfigurationSource myWebsiteConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
-
 	
     @Bean
     public PasswordEncoder passwordEncoder() {
