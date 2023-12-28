14a315a2b67f48001981a3069bd85154f05dd0ba
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-28 00:17:41 +0000

    added admin user

--- src/main/resources/insert-role.sql
+++ src/main/resources/insert-role.sql
@@ -1,3 +1,4 @@
+-- Insert roles if they don't exist
 INSERT INTO roles (name)
 SELECT 'ROLE_ADMIN'
 WHERE NOT EXISTS (
@@ -8,4 +9,24 @@ INSERT INTO roles (name)
 SELECT 'ROLE_USER'
 WHERE NOT EXISTS (
     SELECT 1 FROM roles WHERE name = 'ROLE_USER'
+);
+
+-- Insert admin user if not exists
+INSERT INTO users (username, password)
+SELECT 'khadir', '$2a$12$7J.epa7n2trMae15XPrEDOKlCwY6X6EvjaoA5BwZ04qjzBvTx88ZO'
+WHERE NOT EXISTS (
+    SELECT 1 FROM users WHERE username = 'khadir'
+);
+
+-- Assign ROLE_ADMIN to 'khadir'
+INSERT INTO users_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM users u
+CROSS JOIN roles r
+WHERE u.username = 'khadir' AND r.name = 'ROLE_ADMIN'
+AND NOT EXISTS (
+    SELECT 1 FROM users_roles ur
+    JOIN users u2 ON u2.id = ur.user_id
+    JOIN roles r2 ON r2.id = ur.role_id
+    WHERE u2.username = 'khadir' AND r2.name = 'ROLE_ADMIN'
 );
