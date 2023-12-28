2d0aa9a59f9585d19a8fa6d42e0520faa4eb7c58
Khadir Abdul <khadir786.ka@googlemail.com>
2023-12-28 00:38:10 +0000

    added admin access for admin endpoints

--- src/main/java/com/khadir/pokemonserver/config/SecurityConfig.java
+++ src/main/java/com/khadir/pokemonserver/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,6 +26,7 @@ import com.khadir.pokemonserver.services.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 	
 	private CustomUserDetailsService userDetailsService;
@@ -48,6 +50,7 @@ public class SecurityConfig {
 			.authorizeHttpRequests(authorize -> 
 			authorize
 //					.requestMatchers("/api/auth/register", "/api/users", "/api/auth/login", "/api/**").permitAll()
+					.requestMatchers("/admin/**").hasRole("ADMIN")
 					.anyRequest().permitAll()
 //					.anyRequest().authenticated() // All other requests require authentication
 			)
