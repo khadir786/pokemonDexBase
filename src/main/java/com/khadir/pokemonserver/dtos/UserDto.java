17bd2a9b0148beb4e2414730380bcb0f03556df3
Khadir Abdul <khadir786.ka@googlemail.com>
2024-01-06 17:01:31 +0000

    added age field to user entity and dtos

--- src/main/java/com/khadir/pokemonserver/dtos/UserDto.java
+++ src/main/java/com/khadir/pokemonserver/dtos/UserDto.java
@@ -1,5 +1,9 @@
 package com.khadir.pokemonserver.dtos;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +16,8 @@ public class UserDto {
     private String username;
 	@NotEmpty
 	private String password;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+	private LocalDate dob;
 	private String avatar;
     private String partnerPokemon;
 }
